632cee68fc379817259cce7897e83316ec28cc73
511 <93756134+lananh511@users.noreply.github.com>
2023-11-25 22:27:40 +0700

    update

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,3 +3,8 @@ import { Timestamp } from "firebase/firestore"
 export const toTimestamp = (date: Date): Timestamp => {
     return Timestamp.fromDate(date);
 }
+
+
+export const getNowTime = () : Date => {
+    return new Date();
+}
