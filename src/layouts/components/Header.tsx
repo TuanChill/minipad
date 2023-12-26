3bc0c99bdea1116625b7d7cf87d7819cd5e28474
tuanchill <24a4042615@bav.edu.vn>
2023-12-26 11:22:26 +0700

    add user func in header

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../components/Logo";
 import useWindowDimensions from "../../hooks/useWindowDemensions";
+import Tippy from "@tippyjs/react";
+import { getAuthCache } from "../../containers/localAuth";
+import { isEmptyObject } from "../../utils";
 
 interface NavItem {
   title: string;
@@ -13,34 +16,60 @@ interface IPropsHeader {
 }
 
 export default function Header({ navList, className }: IPropsHeader) {
+  const navigate = useNavigate();
+
+  const user = getAuthCache();
   const { width } = useWindowDimensions();
   return (
     <div
       className={`fixed top-0 w-full bg-slate-50 flex items-center justify-between px-6 py-3 shadow-md ${className}`}
     >
       <Logo />
-      {width >= 750 ? (
-        <nav className="flex gap-4">
-          {navList.map((e) => {
-            return (
-              <Link
-                to={e.path}
-                key={e.title}
-                className="bg-slate-300 p-2 rounded-md font-semibold"
-              >
-                {e.title}
-              </Link>
-            );
-          })}
-        </nav>
-      ) : (
-        <Link
-          to={"/app/pad"}
-          className="bg-slate-300 p-2 rounded-md font-semibold"
-        >
-          Ghi chú ngay
-        </Link>
-      )}
+      <div className="flex items-center">
+        {width >= 750 ? (
+          <nav className="flex gap-4">
+            {navList.map((e) => {
+              return (
+                <Link
+                  to={e.path}
+                  key={e.title}
+                  className="bg-slate-300 p-2 rounded-md font-semibold"
+                >
+                  {e.title}
+                </Link>
+              );
+            })}
+          </nav>
+        ) : (
+          <Link
+            to={"/app/pad"}
+            className="bg-slate-300 p-2 rounded-md font-semibold"
+          >
+            Ghi chú ngay
+          </Link>
+        )}
+        {!isEmptyObject(user) && (
+          <Tippy
+            arrow={true}
+            delay={100}
+            interactive={true}
+            placement="bottom-end"
+            content={<div className="shadow-md flex flex-col gap-1 bg-white border rounded-md py-2 px-1 border-gray-200">
+              <button className="hover:bg-slate-200 px-3 rounded-md" onClick={() => navigate("/profile-setting")}>Tài khoản</button>
+              <button className="hover:bg-slate-200 px-3 rounded-md" onClick={() => navigate("/logout")}>Đăng xuất</button>
+            </div>}
+          >
+            <div className="flex cursor-pointer" onClick={() => navigate("/profile-setting")}>
+              <img
+                className="w-[30px] h-[30px] ml-4 rounded-full"
+                src={user.photoURL}
+                alt={user.fullName}
+              />
+              <i className="ri-arrow-down-s-fill text-2xl"></i>
+            </div>
+          </Tippy>
+        )}
+      </div>
     </div>
   );
 }
--- src/pages/auth/Logout/index.tsx
+++ src/pages/auth/Logout/index.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { auth } from "../../../libs/firebase";
 import { useNavigate } from "react-router-dom";
 import { rmAuthCache } from "../../../containers/localAuth";
+import { logOut } from "../../../services/sign";
 
 export default function Logout() {
   const navigate = useNavigate();
@@ -9,10 +10,12 @@ export default function Logout() {
     auth.signOut().then(
       () => {
         navigate("/login");
+        logOut();
         rmAuthCache();
       },
       () => {
         navigate("/login");
+        logOut();
         rmAuthCache();
       }
     );
