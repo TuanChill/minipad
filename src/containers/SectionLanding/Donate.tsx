29b599b69e830f9018229ad93b80f3e0785c3b20
tuanchill <24a4042615@bav.edu.vn>
2023-12-29 21:23:22 +0700

    donate to me

---
+++ src/containers/SectionLanding/Donate.tsx
@@ -0,0 +1,19 @@
+export default function Donate() {
+  return (
+    <div className="py-16 text-center border-b border-gray-300">
+      <h3 className="title-section">Ủng hộ tôi cốc cà phê... :&gt;</h3>
+      <p className="mt-4">Cảm ơn bạn đã quan tâm</p>
+      <p className="mt-2">
+        Nếu thấy web hay hoặc thương xót có thể ủng hộ tác giả
+      </p>
+      <div className="flex flex-col items-center mt-6">
+        <img
+          className="max-w-[250px]"
+          src="https://firebasestorage.googleapis.com/v0/b/notion-6958d.appspot.com/o/avatars%2Fdonate.jpg?alt=media&token=186dcd62-2cb6-4a0a-8e5d-174072cf7a30"
+          alt="momo"
+        />
+        <a className="mt-2 text-blue-500 underline" href="https://me.momo.vn/G9IwTNsRuVC3C9u2CeiyIb">Momo</a>
+      </div>
+    </div>
+  );
+}
